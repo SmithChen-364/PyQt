2723f6e57a8a3196d176d85dd2eefda6b569461f
Smith Chen <chenyuanswgc@163.com>
2019-08-17 23:19:01 +0800

    example

--- QLabelDemo.py
+++ QLabelDemo.py
@@ -4,21 +4,16 @@ setIndent():设置文本缩进
 text():获取文本内容
 setBuddy():设置伙伴关系
 setText():设置文本内容
-selectedText()：返回所选择的字符
-setWordWrap():设置是否允许换行
-
-QLabel常用的信号（事件）:
-
-1.鼠标滑过控件时触发：linkHovered
-2.鼠标单击控件时触发:linkActivated
+selectedText():返回所选择的字符
+setWordWrap():设置是否换行
 '''
 import sys
-from PyQt5.QtWidgets import QVBoxLayout,QMainWindow,QApplication,QLabel,QPushButton,QWidget
-from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPalette,QPixmap
 from PyQt5.QtCore import Qt
 
 class QLabelDemo(QWidget):
-    def __init__(self):
+    def __init__(self,parent=None):
         super().__init__()
         self.initUI()
     def initUI(self):
@@ -26,32 +21,54 @@ class QLabelDemo(QWidget):
         label2=QLabel(self)
         label3=QLabel(self)
         label4=QLabel(self)
-        label1.setText("<font color=yellow>这是一个文本标签</font>")
-        label.set
+        
+        label1.setText("<font color =yellow>这是一个文本标签</font>")
+        label1.setAutoFillBackground(True)
         palette=QPalette()
-        palette.setColor(QPalette.Window,Qt.blue)
+        #设置背景色
+        palette.setColor(QPalette.Window, Qt.blue)
         label1.setPalette(palette)
         label1.setAlignment(Qt.AlignCenter)
-
-        label2.setText("<a href ='#'>welcome</a>")
+        
+        label2.setText("<a href = '#'>欢迎</a>")
+        
         label3.setAlignment(Qt.AlignCenter)
-        label3.setToolTip("这是一个图片标签")
-        layout=QVBoxLayout()
-        layout.addWidget(label1)
-        layout.addWidget(label2)
-        layout.addWidget(label3)
-
-        label2.linkHovered.connect(self.linkHovered)
-        label3.linkActivated.connect(self.linkClinked)
-
+        
+        label3.setToolTip("picture")
+        label3.setPixmap(QPixmap("./images/NewAI.jpg"))
+        
+        #设置打开外部链接
+        label4.setOpenExternalLinks(True)
+        label4.setText("<a href ='http://www.baidu.com'>百度</a>")
+        label4.setAlignment(Qt.AlignRight)
+        label4.setToolTip("这是一个链接")
+        
+        vbox=QVBoxLayout()
+        vbox.addWidget(label1)
+        vbox.addWidget(label2)
+        vbox.addWidget(label3)
+        vbox.addWidget(label4)
+        
+      
+        
+        label2.linkHovered.connect(self.linkHoverde)
+        label4.linkActivated.connect(self.linkClicked)
+        
+        
+        self.setLayout(vbox)
+        self.setWindowTitle("QLabel控件演示")
+        
+        
+        
+    def linkHoverde(self):
+        print("鼠标滑过")
     
-    def linkHovered(self):
-        print("鼠标正在滑过")
-    def linkClinked(self):
-        print("鼠标正在点击")
+    def linkClicked(self):
+        print("鼠标单击")
+        
 
-if __name__ == "__main__":
-    myapp=QApplication(sys.argv)
-    main=QLabelDemo()
-    main.show()
-    sys.exit(myapp.exec_())
+if __name__ =="__main__":
+    app=QApplication(sys.argv)
+    win=QLabelDemo()
+    win.show()
+    sys.exit(app.exec())
